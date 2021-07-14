ed56b6b56ba87e361624a8e919ccabd4ad674fd8
Matt Thomas <matthewgthomas@gmail.com>
2021-07-14 16:10:09 +0100

    Data wrangling for loneliness question

--- R/explore data.R
+++ R/explore data.R
@@ -15,6 +15,14 @@ categories <-
   slice(-c(1:2)) %>%  # Don't need first two rows
   tidyr::fill(Category, .direction = "down")
 
+# Manually rename a couple of the subcategories
+categories <- 
+  categories %>% 
+  mutate(Subcategory = case_when(
+    Category == "Gender" & Subcategory == "Other" ~ "Other gender",
+    TRUE ~ Subcategory
+  ))
+
 # ---- Experiences of vulnerability/risks/shocks ----
 # Q: In the last three months, have you experienced any of the following? Please tick all that apply.
 experiences <- read_excel("data/BRC Dummy Tables.xlsx", skip = 2, sheet = "OP17272_BRC_Q3")
@@ -82,3 +90,87 @@ lonely <-
   rename(Answer = `...1`) %>% 
   filter(!is.na(Answer)) %>%   # keep only percentages
   filter(Answer != "Return to index")
+
+lonely_long <- 
+  lonely %>% 
+  filter(
+    Answer %in% c("NET: Always/a lot", "NET: Sometimes/hardly ever", "Never", "Prefer not to say")
+  ) %>% 
+  select(
+    Answer, 
+    
+    # Gender
+    Male, Female,
+    
+    # Age
+    `18-34`, `35-54`, `55+`, 
+    
+    # Ethnicity
+    `NET: White background`, 
+    `NET: Mixed / multiple ethnic background`, 
+    `NET: Asian Background`, 
+    `NET: Black/African/Caribbean background`, 
+    `NET: Other`, 
+    `Don’t think of myself as any of these`,
+    `Prefer not to say`,
+    
+    # Urban/rural
+    `Urban area - cities or towns`,
+    `Suburban area – residential areas on the outskirts of cities and towns`,
+    `Rural area - villages or hamlets`,
+    
+    # Social grade
+    ABC1, C2DE,
+    
+    # Working status
+    `Working full time`,
+    `Working part time`, 
+    Retired, 
+    Unemployed, 
+    Other = Other...60,
+    
+    # Disability
+    `Has a disability`, 
+    `Does not have a disability`
+  ) %>% 
+  
+  pivot_longer(cols = -Answer, names_to = "Subcategory", values_to = "Percentage") %>% 
+  
+  left_join(
+    categories,
+    by = "Subcategory"
+  ) %>% 
+  
+  # Rename some of the subcategories
+  mutate(Subcategory = case_when(
+    # Ethnicity
+    Subcategory == "NET: White background" ~ "White",
+    Subcategory == "NET: Mixed / multiple ethnic background" ~ "Mixed/multiple",
+    Subcategory == "NET: Asian Background" ~ "Asian",
+    Subcategory == "NET: Black/African/Caribbean background" ~ "Black/African/Caribbean",
+    Subcategory == "NET: Other" ~ "Other ethnicity",
+    Subcategory == "Don’t think of myself as any of these" ~ "Doesn't identify as any",
+    
+    # Urban/rural
+    Subcategory == "Urban area - cities or towns" ~ "Urban",
+    Subcategory == "Suburban area – residential areas on the outskirts of cities and towns" ~ "Suburban",
+    Subcategory ==  "Rural area - villages or hamlets" ~ "Rural",
+    
+    # Working status
+    Subcategory == "Other...60" ~ "Other",
+    
+    # Disability
+    Subcategory == "Does not have a disability" ~ "No disability",
+    
+    TRUE ~ Subcategory
+  )) %>% 
+  
+  mutate(Answer = str_remove(Answer, "NET: ")) %>% 
+  
+  mutate(Answer = ordered(
+    Answer,
+    levels = c("Prefer not to say", "Never", "Sometimes/hardly ever", "Always/a lot")
+  )) %>% 
+  
+  relocate(Category, Subcategory, Answer, Percentage)
+  
