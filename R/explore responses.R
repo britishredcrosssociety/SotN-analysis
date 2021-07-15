63914a3b5863bf8003709785c225e44938c2dadd
Matt Thomas <matthewgthomas@gmail.com>
2021-07-15 17:34:44 +0100

    Started on analysis of monetary support

--- R/explore responses.R
+++ R/explore responses.R
@@ -51,3 +51,28 @@ all_events %>%
   adorn_pct_formatting(digits = 1)
 
 # ---- Routes to support ----
+# Money
+all_support_money <- 
+  all %>% 
+  select(
+    altid.a, 
+    Ethnicity = OP17272_BRC_Q2.a,
+    starts_with("OP17272_BRC_Q4")
+  ) %>% 
+  
+  # Drop columns for "None of the above" and "Prefer not to say"
+  select(-c(OP17272_BRC_Q4.a.11, OP17272_BRC_Q4.a.12)) %>% 
+  
+  rowwise() %>% 
+  mutate(n_events = sum(!is.na(c_across(starts_with("OP17272_BRC_Q4")))))
+
+# Bin the counts of adverse events (in a fairly arbitrary way) and summarise
+all_events %>% 
+  mutate(n_events_grouped = case_when(
+    n_events == 0 ~ "0",
+    n_events == 1 ~ "1",
+    n_events > 1 & n_events <= 5 ~ "2-5",
+    n_events > 5 ~ "5+"
+  )) %>% 
+  
+  tabyl(n_events)
